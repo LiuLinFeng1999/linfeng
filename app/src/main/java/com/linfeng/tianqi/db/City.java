af4ab0cf87de35e8e26af0441963776dfd875df1
LiuLinFeng1999 <1961277046@qq.com>
2018-12-20 14:18:58 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/linfeng/tianqi/db/City.java
@@ -0,0 +1,43 @@
+package com.linfeng.tianqi.db;
+import org.litepal.crud.*;
+
+public class City extends DataSupport
+{
+	private int id;
+	private String cityName;
+	private int cityCode;
+	private int provinceId;
+
+	public void setCityCode(int cityCode)
+	{
+		this.cityCode = cityCode;
+	}
+
+	public int getCityCode()
+	{
+		return cityCode;
+	}
+
+	public void setProvinceId(int provinceId)
+	{
+		this.provinceId = provinceId;
+	}
+
+	public int getProvinceId()
+	{
+		return provinceId;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id=id;
+	}
+	public String getCityName(){
+		return cityName;
+	}
+	public void setCityName(String cityName){
+		this.cityName=cityName;
+	}
+}
---
+++ app/src/main/java/com/linfeng/tianqi/db/County.java
@@ -0,0 +1,51 @@
+package com.linfeng.tianqi.db;
+import org.litepal.crud.*;
+
+public class County extends DataSupport
+{
+	private int id;
+	private String countyName;
+	private String weatherId;
+	private int cityId;
+
+	public void setCityId(int cityId)
+	{
+		this.cityId = cityId;
+	}
+
+	public int getCityId()
+	{
+		return cityId;
+	}
+
+	public void setWeatherId(String weatherId)
+	{
+		this.weatherId = weatherId;
+	}
+
+	public String getWeatherId()
+	{
+		return weatherId;
+	}
+
+	public void setCountyName(String countyName)
+	{
+	this.countyName = countyName;
+	}
+
+	public String getCountyName()
+	{
+	return countyName;
+	}
+	
+
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public int getId()
+	{
+		return id;
+	}}
